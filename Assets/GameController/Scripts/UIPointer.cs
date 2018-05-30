e40926c10fff9023c71afc3d6fd6392f20ea7b3c
senrik <s.erik.swanson@gmail.com>
2018-05-30 17:38:05 -0400

    Added the a "Starter Vive" scene.
    Able to pick up objects, and Teleport around the scene.

---
+++ Assets/GameController/Scripts/UIPointer.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameController
+{
+    public class UIPointer : MonoBehaviour
+    {
+
+        public GameObject pointerModelPrefab;
+
+        private GameObject pointerModel;
+        private Vector3 hitPoint;
+        private SteamVR_TrackedObject trackedObj;
+        private SteamVR_Controller.Device Controller
+        {
+            get { return SteamVR_Controller.Input((int)trackedObj.index); }
+        }
+
+        private void Awake()
+        {
+            trackedObj = GetComponent<SteamVR_TrackedObject>();
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+            pointerModel = Instantiate(pointerModelPrefab);
+        }
+
+        void ShowPointer(RaycastHit hit)
+        {
+            pointerModel.SetActive(true);
+
+            pointerModel.transform.position = Vector3.Lerp(trackedObj.transform.position, hitPoint, 0.5f);
+
+            pointerModel.transform.LookAt(hitPoint);
+
+            pointerModel.transform.localScale = new Vector3(pointerModel.transform.localScale.x, pointerModel.transform.localScale.y, hit.distance /*Mathf.Infinity*/);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            /* If this controller's hair trigger is being pressed. */
+            if(Controller.GetHairTriggerDown())
+            {
+                /* Cast a ray with a UI layer-mask. */
+                RaycastHit hit;
+
+                if(Physics.Raycast(trackedObj.transform.position, transform.forward, out hit))
+                {
+                    hitPoint = hit.point;
+                    ShowPointer(hit);
+                }
+                
+            }
+        }
+    }
+}
+
---
+++ Assets/ViveStarterKit/Scripts/LaserPointer.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserPointer : MonoBehaviour {
+
+    public ViveControllerInputTest inputHandler;
+    public GameObject laserPrefab;
+
+    public Transform cameraRigTransform;
+    public GameObject teleportReticlePrefab;
+
+    private GameObject reticle;
+    private Transform teleportReticleTransform;
+
+    public Transform headTransform;
+
+    public Vector3 teleportReticleOffset;
+    public LayerMask teleportMask;
+
+    private bool shouldTeleport;
+
+    private GameObject laser;
+
+    private Transform laserTransform;
+    private Vector3 hitPoint;
+    private Vector3 laserScale;
+    private SteamVR_Controller.Device Controller
+    {
+        get { return inputHandler.Stick; }
+    }
+
+    void Awake()
+    {
+        if(GetComponent<ViveControllerInputTest>())
+        {
+            inputHandler = GetComponent<ViveControllerInputTest>();
+        }
+        laserScale = new Vector3();
+    }
+
+    private void Start()
+    {
+        laser = Instantiate(laserPrefab);
+        laserTransform = laser.transform;
+
+        reticle = Instantiate(teleportReticlePrefab);
+
+        teleportReticleTransform = reticle.transform;
+    }
+
+    private void ShowLaser(RaycastHit hit)
+    {
+        laser.SetActive(true);
+        laserTransform.position = Vector3.Lerp(transform.position, hitPoint, 0.5f);
+
+        laserTransform.LookAt(hitPoint);
+
+        laserScale = laserTransform.localScale;
+        laserScale.z = hit.distance;
+
+        laserTransform.localScale = laserScale;
+    }
+
+    private void Teleport()
+    {
+        shouldTeleport = false;
+
+        Vector3 difference = cameraRigTransform.position - headTransform.position;
+
+        difference.y = 0;
+
+        cameraRigTransform.position = hitPoint + difference;
+    }
+
+	// Update is called once per frame
+	void Update () {
+		if(Controller.GetPress(SteamVR_Controller.ButtonMask.Touchpad))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, transform.forward, out hit, 100, teleportMask))
+            {
+                hitPoint = hit.point;
+                ShowLaser(hit);
+
+                reticle.SetActive(true);
+
+                teleportReticleTransform.position = hitPoint + teleportReticleOffset;
+
+                shouldTeleport = true;
+            }
+        }
+        else
+        {
+            laser.SetActive(false);
+
+            reticle.SetActive(false);
+        }
+
+        if(Controller.GetPressUp(SteamVR_Controller.ButtonMask.Touchpad) && shouldTeleport)
+        {
+            Teleport();
+        }
+	}
+}
