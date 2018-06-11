90a516f5187dbccc1c9487395122c063b9da63c9
senrik <s.erik.swanson@gmail.com>
2018-06-11 07:50:56 -0400

    Added components to the LaserPointer script to include UI interactions.

--- Assets/ViveStarterKit/Scripts/LaserPointer.cs
+++ Assets/ViveStarterKit/Scripts/LaserPointer.cs
@@ -6,6 +6,7 @@ public class LaserPointer : MonoBehaviour {
 
     public ViveControllerInputTest inputHandler;
     public GameObject laserPrefab;
+    public GameObject uiPointerPrefab;
 
     public Transform cameraRigTransform;
     public GameObject teleportReticlePrefab;
@@ -17,12 +18,13 @@ public class LaserPointer : MonoBehaviour {
 
     public Vector3 teleportReticleOffset;
     public LayerMask teleportMask;
+    public LayerMask uiMask;
 
-    private bool shouldTeleport;
+    private bool shouldTeleport, uiElement;
 
-    private GameObject laser;
-
-    private Transform laserTransform;
+    private GameObject laser, uiLaser;
+    private GameObject selectedUIElement;
+    private Transform laserTransform, uiLaserTransform;
     private Vector3 hitPoint;
     private Vector3 laserScale;
     private SteamVR_Controller.Device Controller
@@ -42,7 +44,10 @@ public class LaserPointer : MonoBehaviour {
     private void Start()
     {
         laser = Instantiate(laserPrefab);
+        uiLaser = Instantiate(uiPointerPrefab);
+
         laserTransform = laser.transform;
+        uiLaserTransform = uiLaser.transform;
 
         reticle = Instantiate(teleportReticlePrefab);
 
@@ -61,6 +66,22 @@ public class LaserPointer : MonoBehaviour {
 
         laserTransform.localScale = laserScale;
     }
+    private void ShowUILaser(RaycastHit hit)
+    {
+        if(laser.activeSelf)
+        {
+            laser.SetActive(false);
+        }
+        uiLaser.SetActive(true);
+        uiLaserTransform.position = Vector3.Lerp(transform.position, hitPoint, 0.5f);
+
+        uiLaserTransform.LookAt(hitPoint);
+
+        laserScale = uiLaserTransform.localScale;
+        laserScale.z = hit.distance;
+
+        uiLaserTransform.localScale = laserScale;
+    }
 
     private void Teleport()
     {
@@ -71,6 +92,16 @@ public class LaserPointer : MonoBehaviour {
         difference.y = 0;
 
         cameraRigTransform.position = hitPoint + difference;
+    }
+
+    private void SelectUIElement()
+    {
+        if (selectedUIElement)
+        {
+            Debug.Log(selectedUIElement.name + " selected.");
+            selectedUIElement = null;
+        }
+
     }
 
 	// Update is called once per frame
@@ -89,17 +120,34 @@ public class LaserPointer : MonoBehaviour {
 
                 shouldTeleport = true;
             }
+            if (Physics.Raycast(transform.position, transform.forward, out hit, 100, uiMask))
+            {
+                hitPoint = hit.point;
+                ShowUILaser(hit);
+                selectedUIElement = hit.collider.gameObject;
+
+                uiElement = true;
+            }
+            else
+            {
+                uiLaser.SetActive(false);
+                selectedUIElement = null;
+            }
         }
         else
         {
             laser.SetActive(false);
-
+            uiLaser.SetActive(false);
             reticle.SetActive(false);
         }
 
         if(Controller.GetPressUp(SteamVR_Controller.ButtonMask.Touchpad) && shouldTeleport)
         {
             Teleport();
+        }
+        if (Controller.GetPressUp(SteamVR_Controller.ButtonMask.Touchpad) && uiElement)
+        {
+            SelectUIElement();
         }
 	}
 }
